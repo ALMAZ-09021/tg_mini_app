35128cfbb49f8d91e0e7046e09a5b5be04d2a27d
ALMAZ-09021 <almaz.khaybullin@mail.ru>
2025-11-28 17:04:48 +0300

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Разрешаем этот конкретный домен
-    allowedHosts: ['superglottal-yvone-unreprehended.ngrok-free.dev'],
-
-    // ИЛИ просто разрешаем всё (проще):
-    // allowedHosts: true,
-  },
+  // base: '/tg_mini_app/', ← удалите эту строку или замените на:
+  base: '/',
 })
